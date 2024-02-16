8b5c85cd0e113d6eef6e712b90f849a9c18db4a3
Zaidin Amiot <zr0z@me.com>
2024-02-16 15:01:44 +0900

    :art: Add tako10

---
+++ keyboards/takomecha/tako10/config.h
@@ -0,0 +1,8 @@
+// Copyright 2023 Zaidin Amiot (@takomecha)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define NOP_FUDGE 0.4
+#define RGB_DI_PIN GP3
+#define RGB_MATRIX_LED_COUNT 10
---
+++ keyboards/takomecha/tako10/keymaps/default/keymap.c
@@ -0,0 +1,5 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
+    // ASET NIOP
+    {[0] = LAYOUT_row_10(KC_A, KC_S, KC_E, KC_T, RGB_TOG, KC_SPC, KC_N, KC_I, KC_O, KC_P)};
